098effebc266cced572d355df6d6ca548c120793
piyush97 <me@piyushmehta.com>
2022-01-28 15:43:54 -0500

    setup files

---
+++ constants.js
@@ -0,0 +1,11 @@
+/**
+ * @fileOverview This file contains the constants used in the application.
+ * @author Piyush97
+ * @version 0.0.1
+ *
+ */
+export const APP_DATA = {
+  name: "0xZ3ST",
+  version: "0.0.1",
+  description: `The Treasure hunt with Metaverse flavour.`,
+};
