25c194632906cbeadd3607e5391b4164930865a3
piyush97 <me@piyushmehta.com>
2022-01-28 15:37:04 -0500

    fix: index startup process with data [Constant]

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 import express from "express";
 
+/** @type {Express}
+ *  @description The express application.
+ *  @see {@link https://expressjs.com/en/4x/api.html#express}
+ *
+ */
 const app = express();
 
+/** @type {number}
+ * @description port number
+ * @default 3000
+ */
 const PORT = process.env.PORT || 3000;
+
 // view engine to ejs
 app.set("view engine", "ejs");
 
-// index page
+// Initial routing
 app.get("/", function (req, res) {
-  // using response => render to load up an ejs view file
-  res.render("pages/index");
+  res.render("pages/index", {
+    name: "0xZ3ST",
+    version: "0.0.1",
+    description: `The Treasure hunt with Metaverse flavour.`,
+  });
 });
 
 app.listen(PORT, function () {
-  console.log("Server started on port " + PORT);
+  console.log("Server started on port " + PORT + " 🚀");
 });
