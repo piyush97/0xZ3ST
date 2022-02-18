315d5f51ddca757cbb94d4580ba425aea762dd88
Ekta Kaushik <ektakaushik3103@gmail.com>
2022-02-18 01:34:35 -0500

    splash screen review

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let Intro = document.querySelector('.Intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+
+
+
 // Firebase Configuration will be provided by Piyush
 var firebaseConfig = {
   // Paste credentials here
@@ -137,3 +144,38 @@ function validate_field(field) {
 
 
 }
+window.addEventListener('DOMContentLoaded',()=>
+
+{
+      setTimeout(()=>{
+
+
+        logoSpan.forEach((span,idx)=>{
+          setTimeout(()=>{
+            span.classList.add('active');
+          }, (idx+1 )*400)
+        });
+
+          setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+              setTimeout(()=>{
+
+                span.classList.remove('active');
+                span.classList.add('fade');
+              },(idx+1)*50)
+
+              })
+            },2000);
+
+
+
+          setTimeout(()=>{
+
+              Introduction.style.top ='-100vh';
+          },2300)
+
+
+          })
+            
+})
